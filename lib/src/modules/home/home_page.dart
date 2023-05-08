67940e11965d5a0846fe00a2cbab7bc4316fe57a
nbthales <nbthales@gmail.com>
2023-05-08 11:24:33 -0300

    00:48:27

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
 
 import '../../core/env/env.dart';
+import '../../core/ui/helpers/loader.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Container(
-            child: Text(Env.instance.get('backend_base_url')),
-           ),
-       );
+class _HomePageState extends State<HomePage> with Loader {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() async {
+          showLoader();
+          await Future.delayed(const Duration(seconds: 2));
+          hideLoader();
+        }),
+      ),
+      body: Container(
+        child: Text(Env.instance.get('backend_base_url')),
+      ),
+    );
   }
 }
