2892bbe060dbfe6add32f507aa9398d8231cabb5
nbthales <nbthales@gmail.com>
2023-05-10 11:26:11 -0300

    00:53:31

--- lib/src/repositories/payment_type/payment_type_repository_impl.dart
+++ lib/src/repositories/payment_type/payment_type_repository_impl.dart
@@ -16,7 +16,7 @@ class PaymentTypeRepositoryImpl implements PaymentTypeRepository {
   Future<List<PaymentTypeModel>> findAll(bool? enabled) async {
     try {
       final paymentResult = await _dio.auth().get(
-        '/payment-type',
+        '/payment-types',
         queryParameters: {
           if (enabled != null) 'enabled': enabled,
         },
@@ -34,7 +34,7 @@ class PaymentTypeRepositoryImpl implements PaymentTypeRepository {
   Future<PaymentTypeModel> getById(int id) async {
     try {
       final paymentResult = await _dio.auth().get(
-            '/payment-type/$id',
+            '/payment-types/$id',
           );
       return PaymentTypeModel.fromMap(paymentResult.data);
     } on DioError catch (e, s) {
@@ -52,12 +52,12 @@ class PaymentTypeRepositoryImpl implements PaymentTypeRepository {
 
       if (model.id != null) {
         await client.put(
-          '/payment-type/${model.id}',
+          '/payment-types/${model.id}',
           data: model.toMap(),
         );
       } else {
         await client.post(
-          '/payment-type/',
+          '/payment-types/',
           data: model.toMap(),
         );
       }
